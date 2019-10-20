ac9069880e8c738e85e313e311f05ca80e40356a
Zachery Morris <ztm4qv@virginia.edu>
2019-10-20 17:33:54 -0400

    Fixing oauth

--- uvacraigslist/urls.py
+++ uvacraigslist/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     # path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path(r'oauth/', include('social_django.urls', namespace='social')),
+    path('auth/', include('social_django.urls', namespace='social')),
     path('logout/', logout, {'next_page': settings.LOGOUT_REDIRECT_URL}, name='logout'),
 ]
