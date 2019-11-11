87ac360757e7ff77b981f19d4af1e8b254b75d81
rqhrdy <54818825+rqhrdy@users.noreply.github.com>
2019-11-11 10:41:03 -0500

    Update views.py

--- marketplace/views.py
+++ marketplace/views.py
@@ -72,7 +72,7 @@ def filter(request):
 
 def inbox(request):
     context = {
-        'messages': Message.objects.filter(receiver=request.user.username)
+        'messages': Message.objects.filter(receiver=request.user)
     }
     return render(request, 'marketplace/inbox.html', context)
 
