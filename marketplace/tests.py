fc006602c87027408dc0d458d0cebf0eace9d18b
Caroline <ch6yg@virginia.edu>
2019-11-11 10:41:22 -0500

    test1

--- marketplace/tests.py
+++ marketplace/tests.py
@@ -12,8 +12,13 @@ from .views import index, detail, add_item
 # use search bar
 # add an item
 # view a detailed
+class not404Test(TestCase):
+    def test_searchpage(self):
+        resp = self.client.get()
+
 
 class addItemTest(TestCase):
+    #1
     def test_to_add_item(self):
         i = Item()
         s = Seller()
@@ -33,6 +38,7 @@ class addItemTest(TestCase):
         record = Item.objects.get(pk=1)
         self.assertEquals(record, i)
 
+    #2
     @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
     def test_to_add_page(self):
         resp = self.client.get('/marketplace/add-item/')
@@ -41,17 +47,21 @@ class addItemTest(TestCase):
 
 
 class homeTest(TestCase):
+    #3
     @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
     def test_home(self):
         resp = self.client.get('')
         self.assertEquals(resp.status_code, 200)
 
+    #4
     @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
     def test_index(self):
         resp = self.client.get('/marketplace/')
         self.assertTemplateUsed(resp, 'marketplace/index.html')
+        self.assertContains(resp, "Items currently for sale")
 
 class searchTest(TestCase):
+    #5
     @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
     def test_search(self):
         i = Item()
@@ -73,6 +83,7 @@ class searchTest(TestCase):
         resp = self.client.get('/marketplace/search/', {'q' : 'table'} )
         self.assertEquals(resp.status_code, 200)
 
+        #6
     @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
     def test_search_2(self):
         i = Item()
@@ -94,3 +105,162 @@ class searchTest(TestCase):
 
         resp = self.client.get('/marketplace/search/', {'q' : 'table'} )
         self.assertContains(resp, 'Big Table')
+    #7
+    @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+    def test_search_3(self):
+        i = Item()
+        s = Seller()
+
+        s.seller_name = "Caroline"
+        s.seller_computing_id = "ch6yg"
+        s.save()
+        i.item_name = "Big Table"
+        i.seller = s
+        i.item_description = "wooden, large table for sale"
+        i.item_price = "100"
+        i.item_location = "uva"
+        i.item_condition = 'GOOD'
+        i.item_categories = "FURNITURE"
+        i.item_preferred_payment_method ="venmo"
+        i.save()
+
+        i2 = Item()
+        i2.item_name = "Tshirt"
+        i2.seller = s
+        i2.item_description = "large  blue tshirt "
+        i2.item_price = "100"
+        i2.item_location = "uva"
+        i2.item_condition = 'GOOD'
+        i2.item_categories = "CLOTHING"
+        i2.item_preferred_payment_method ="venmo"
+        i2.save()
+        resp = self.client.get('/marketplace/search/', {'q' : 'table'} )
+        self.assertFalse('tshirt' in resp)
+        #self.assertContains(resp, 'tshirt')
+
+class filterTest(TestCase):
+
+    @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+    def setUp(self):
+        i = Item()
+        s = Seller()
+
+        s.seller_name = "Caroline"
+        s.seller_computing_id = "ch6yg"
+        s.save()
+        i.item_name = "Big Table"
+        i.seller = s
+        i.item_description = "wooden, large table for sale"
+        i.item_price = "100"
+        i.item_location = "uva"
+        i.item_condition = 'GOOD'
+        i.item_categories = "FURNITURE"
+        i.item_preferred_payment_method ="venmo"
+        i.save()
+
+        i2 = Item()
+        i2.item_name = "Tshirt"
+        i2.seller = s
+        i2.item_description = "large  blue tshirt "
+        i2.item_price = "100"
+        i2.item_location = "uva"
+        i2.item_condition = 'GOOD'
+        i2.item_categories = "CLOTHING"
+        i2.item_preferred_payment_method ="venmo"
+        i2.save()
+    #8
+    @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+    def test_filter(self):
+        resp = self.client.get('/marketplace/filter/', {'f': 'FURNITURE'})
+        self.assertContains(resp, "Big Table")
+    #9
+    @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+    def test_filter2(self):
+        resp  = self.client.get('/marketplace/filter/', {'f': 'CLOTHING'})
+        self.assertContains(resp, "Tshirt")
+    #10
+    @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+    def test_filter3(self):
+        resp = self.client.get('/marketplace/filter/', {'f': 'CLOTHING'})
+        self.assertFalse("table" in resp)
+
+
+
+
+class detailTest(TestCase):
+    #11
+    @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+    def test_detail(self):
+        i = Item()
+        s = Seller()
+
+        s.seller_name = "Caroline"
+        s.seller_computing_id = "ch6yg"
+        s.save()
+        i.item_name = "Big Table"
+        i.seller = s
+        i.item_description = "wooden, large table for sale"
+        i.item_price = "100"
+        i.item_location = "uva"
+        i.item_condition = 'GOOD'
+        i.item_categories = "FURNITURE"
+        i.item_preferred_payment_method ="venmo"
+        i.save()
+
+        i2 = Item()
+        i2.item_name = "Tshirt"
+        i2.seller = s
+        i2.item_description = "large  blue tshirt "
+        i2.item_price = "100"
+        i2.item_location = "uva"
+        i2.item_condition = 'GOOD'
+        i2.item_categories = "CLOTHING"
+        i2.item_preferred_payment_method ="venmo"
+        i2.save()
+
+
+        resp = self.client.get(reverse('marketplace:detail', args = (i.id,)))
+        self.assertContains(resp, 'table')
+    #12
+    @override_settings(STATICFILES_STORAGE='django.contrib.staticfiles.storage.StaticFilesStorage')
+    def test_detail2(self):
+        i = Item()
+        s = Seller()
+
+        s.seller_name = "Caroline"
+        s.seller_computing_id = "ch6yg"
+        s.save()
+        i.item_name = "Big Table"
+        i.seller = s
+        i.item_description = "wooden, large table for sale"
+        i.item_price = "100"
+        i.item_location = "uva"
+        i.item_condition = 'GOOD'
+        i.item_categories = "FURNITURE"
+        i.item_preferred_payment_method ="venmo"
+        i.save()
+
+        i2 = Item()
+        i2.item_name = "Tshirt"
+        i2.seller = s
+        i2.item_description = "large  blue tshirt "
+        i2.item_price = "100"
+        i2.item_location = "uva"
+        i2.item_condition = 'GOOD'
+        i2.item_categories = "CLOTHING"
+        i2.item_preferred_payment_method ="venmo"
+        i2.save()
+        resp = self.client.get(reverse('marketplace:detail', args = (i2.id,)))
+        self.assertFalse('table' in resp)
+
+
+'''
+class userRatingTest(TestCase):
+    def test_rating(self):
+
+class imageTest(TestCase):
+    '''
+
+# login tests?
+
+#messaging tests
