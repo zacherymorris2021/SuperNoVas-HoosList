428d50153f46f6e5ab150a4b2365a840e79994e6
Catherine Im <ci6nq@virginia.edu>
2019-12-02 01:22:55 -0500

    item model update

--- marketplace/models.py
+++ marketplace/models.py
@@ -16,7 +16,7 @@ class Seller(models.Model):
     negRate=models.IntegerField(default=0)
     numTransactions = models.IntegerField(default=0)
 
-    
+
 
 class Item(models.Model):
     item_name = models.CharField (max_length=200)
@@ -29,7 +29,7 @@ class Item(models.Model):
     item_preferred_payment_method = models.CharField(max_length=10, choices=PAYMENT_METHODS, default='venmo')
     item_condition = models.CharField(max_length=10, choices=CONDITION_CHOICES, default='good')
     item_sold = models.BooleanField(default=False) #private for admins and seller
-    item_categories = MultiSelectField(max_length=83, choices=CATEGORIES)
+    item_categories = models.CharField(max_length=100, choices=CATEGORIES)
     item_add_date = models.DateTimeField('date published', default=datetime.now)
     latitude = models.FloatField(default=38.035618)
     longitude =  models.FloatField(default=-78.503415)
